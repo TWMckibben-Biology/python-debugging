ed14ba010f13aff447b2d594b8f17a10371ef5d0
TaylorMckibben <twm0009@auburn.edu>
2023-02-25 19:53:03 -0600

    Debugged code. Had to convert variables to integers. Messed around with where things were located in the script and if they were throwing errors.

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -29,9 +29,7 @@ def area_of_rectangle(height, width = None):
     >>> area_of_rectangle (7, 2)
     14
     """
-    if width:
-        width = height
-    area = height * width
+    area = int(height) * int(width)
     return area
 
 if __name__ == '__main__':
@@ -41,10 +39,14 @@ if __name__ == '__main__':
                 "\tthe height of a square or the height and width of a "
                 "rectangle".format(script_name = sys.argv[0]))
         sys.exit(message)
-    height = sys.argv[1]
+    height = (sys.argv[1])
     width = height
-    if len(sys.argv) > 3:
-        width = sys.argv[1]
+    
+    if len(sys.argv) == 2:
+        width = height
+
+    if len(sys.argv) > 2:
+        width = sys.argv[2]
 
     area = area_of_rectangle(height, width)
 
